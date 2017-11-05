057094b15f080cd740160f9acadd7c29d2c002d3
yesheng <dasheng523@163.com>
2017-11-05 22:12:33 +0800

    config

--- src/grapdata/toutiao/config.clj
+++ src/grapdata/toutiao/config.clj
@@ -1,10 +1,11 @@
 (ns grapdata.toutiao.config)
 
-(def platform "mac")
-(def download-base-path "/Users/huangyesheng/Documents/pics")
-(def url-data "/Users/huangyesheng/Documents/pics/test.txt")
-(def cookies-base-path "/Users/huangyesheng/Documents/cookies/")
+#_(def platform "mac")
+#_(def download-base-path "/Users/huangyesheng/Documents/pics")
+#_(def url-data "/Users/huangyesheng/Documents/pics/test.txt")
+#_(def cookies-base-path "/Users/huangyesheng/Documents/cookies/")
 
-#_(def platform "win")
-#_(def download-base-path "e:\\pics")
-#_(def url-data "e:\\test.txt")
+(def platform "win")
+(def download-base-path "e:\\pics")
+(def url-data "e:\\test.txt")
+(def cookies-base-path "e:\\cookies\\")
